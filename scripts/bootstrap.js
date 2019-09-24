9bb1bb930c12c5ff27befcf9489a6af722ad7226
Artsiom Peshko <Artsiom_Peshko@epam.com>
2019-09-24 11:35:48 +0300

    added test pages

---
+++ scripts/bootstrap.js
@@ -0,0 +1,64 @@
+(() => {
+  const urls = {
+    dev: {
+      widget: 'https://dev-dsg11-www.carcodesms.com/widgets/:dealerId.js',
+      inventories: 'https://dev-ext-www.carcode.com/carcode/v1/ccapi/dealer/:rooftopId/inventories/widget/search?pagesize=5&pagenum=1',
+      inventory: 'http://qa-11-www.edmunds.com/api/inventory/v5/find',
+    },
+    qa: {
+      widget: 'https://qa-dsg11-www.carcodesms.com/widgets/:dealerId.js',
+      inventories: 'https://qa-ext-www.carcode.com/carcode/v1/ccapi/dealer/:rooftopId/inventories/widget/search?pagesize=5&pagenum=1',
+      inventory: 'http://qa-11-www.edmunds.com/api/inventory/v5/find',
+    },
+    prod: {
+      widget: 'https://www.carcodesms.com/widgets/:dealerId.js',
+      inventories: 'https://www.carcode.com/carcode/v1/ccapi/dealer/:rooftopId/inventories/widget/search?pagesize=5&pagenum=1',
+      inventory: 'http://www.edmunds.com/api/inventory/v5/find',
+    },
+  };
+
+  window.carcodeUrls = urls;
+
+  const loadScript = (url, onLoad) => {
+    // render widgets/tracker
+    const script = document.createElement('script');
+
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = url;
+
+    if (onLoad) {
+      script.addEventListener('load', onLoad, false);
+    }
+
+    script.addEventListener('error', () => {
+      alert(`The following script ${url} failed to load. Please recheck configuration (ctrl + p) and network connection.`)
+    }, false);
+
+    document.getElementsByTagName('script')[0].parentNode.insertBefore(script, document.getElementsByTagName('script')[0]);
+  };
+
+  const loadWidget = () => {
+    if (window.carcodeSettingsData.sdk) {
+      window.__carcode = window.carcodeSettingsData.sdk;
+    }
+
+    loadScript(urls[window.carcodeSettingsData.env].widget.replace(':dealerId', window.carcodeSettingsData.dealerId));
+  };
+
+  const loadEva = () => {
+    loadScript('https://content-container.edmunds.com/850449.js');
+  };
+
+  const init = () => {
+    if (!window.carcodeSettingsData) {
+      return;
+    }
+
+
+    loadWidget();
+    loadEva();
+  };
+
+  init();
+})();
---
+++ scripts/settings.js
@@ -0,0 +1,139 @@
+(() => {
+  const loadForm = async () => fetch('/pages/settings-form')
+    .then(response => response.text())
+    .then((formHtml) => {
+      $('body').append(formHtml);
+    });
+
+  const toggleModal = () => {
+    $('#settingsModal').modal('toggle');
+  };
+
+  const submitModal = () => {
+    if(!$('#settingsModalForm')[0].checkValidity()) {
+      $('#settingsModalForm')[0].classList.add('was-validated');
+    } else {
+      save();
+      toggleModal();
+
+      // navigate to home
+      if (location.href.includes('settings.html')) {
+        window.location.href = '/pages/home.html';
+      } else { // reload page
+        location.reload();
+      }
+    }
+  };
+
+  const listenShortcuts = () => {
+    hotkeys('ctrl+p', (event, handler) => {
+      switch (handler.key) {
+        case 'ctrl+p': {
+          toggleModal();
+          event.preventDefault();
+          break;
+        }
+      }
+    });
+  };
+
+  const listenActions = () => {
+    $('#modalSave').click((e) => {
+      e.preventDefault();
+
+      submitModal();
+    });
+
+    $('#settingsModalForm').submit((e) => {
+      e.preventDefault();
+
+      submitModal();
+    });
+  }
+
+  const listenSdk = () => {
+    $('#sdk input')
+      .change(() => {
+        $('#sdkJsonInput').val()
+      });
+  };
+
+  const getSettings = () => JSON.parse(window.localStorage.getItem('carcode_test_page_settings'));
+
+  const extractEnv = () => $('#environment input:checked').val();
+
+  const extractDealerId = () => $('#dealerIdInput').val();
+
+  const extractRooftopId = () => $('#rooftopIdInput').val();
+
+  const extractSdkSettings = () => {
+    let result = {};
+
+    if ($('#sdkJsonInput').val()) {
+      result = JSON.parse($('#sdkJsonInput').val());
+    }
+
+    return result;
+  };
+
+  const extractForm = () => {
+    const result = {
+      env: extractEnv(),
+      dealerId: extractDealerId(),
+      rooftopId: extractRooftopId(),
+      sdk: extractSdkSettings(),
+    };
+
+    return result;
+  }
+
+  const save = () => {
+    const settingsData = extractForm();
+
+    window.localStorage.setItem('carcode_test_page_settings', JSON.stringify(settingsData));
+  };
+
+  const setFormData = () => {
+    const settingsData = getSettings();
+
+    if (!settingsData) {
+      return;
+    }
+
+    $('#dealerIdInput').val(settingsData.dealerId);
+    $('#rooftopIdInput').val(settingsData.rooftopId);
+    $(`input[name=gridRadios][value=${settingsData.env}]`).prop('checked',true);
+
+    if (settingsData.sdk) {
+      $('#sdkJsonInput').val(JSON.stringify(settingsData.sdk));
+    }
+  }
+
+  const firstVisit = () => {
+    $('#settingsModal').modal({ backdrop: 'static', keyboard: false });
+    toggleModal();
+    $('#modalClose').hide();
+  }
+
+  const init = async () => {
+    // init settings global variable
+    window.carcodeSettingsData = getSettings();
+
+    // load form html
+    await loadForm();
+
+    // first opennning
+    if (location.href.includes('settings.html')
+      || Object.keys(window.carcodeSettingsData || {}).length === 0) {
+      firstVisit();
+    }
+
+    // init form actions
+    listenShortcuts();
+    listenActions();
+    listenSdk();
+    setFormData();
+  };
+
+  init();
+})();
